9caa802b18315af7bd837e8b6022dbb518df7be5
Yorok0bi <vincentconq.pro@gmail.com>
2023-11-24 15:58:31 +0100

    modifv2 render.yaml

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-process.env.database_url,
+process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
